87e89f4a21eb007360028f2ceabd4866a25b71fe
Artur Khasbiev <Khasbiich@yandex.ru>
2020-10-19 21:07:33 +0300

    init: start basic-js task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
+	if (typeof(members) === 'object' && (members !== null)) {
+	let nameArray = [];
+  for (let i = 0; i < members.length; i++){
+if (typeof(members[i]) === 'string') {
+  members[i] = members[i].trim();
+	nameArray.push(members[i][0].toUpperCase());
+}
+  }
+  return nameArray.sort().join('');
+}
+return false;
   // remove line with error and write your code here
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+   if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+  let stack = [];
+    for ( let i = 0; i < arr.length; i++){
+     if (arr[i] === '--discard-next'){
+       if (arr[i + 2] !== '--discard-prev'){
+          if(i !== arr.length - 1){
+        i += 1;
+          }
+       }
+
+     } else if (arr[i] === '--discard-prev'){
+      stack.pop();
+     } else if (arr[i] === '--double-next'){
+      if(arr[i + 1] !== undefined){
+      stack.push(arr[i + 1]);
+      }
+     } else if (arr[i] === '--double-prev'){
+      if(arr[i - 1] !== undefined && arr[i - 2] !== '--discard-next'){
+      stack.push(arr[i - 1]); 
+      } 
+     } else {
+       stack.push(arr[i]);
+     }
+  }
+  console.log(arr);
+  return stack;
 };
